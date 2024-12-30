fbaea9e6eeb8ac0a5b939979b08b050fd13e4d3b
farelaryaduta <farelminecraft450@gmail.com>
2024-12-30 12:03:30 +0700

    mengubah payment.php

--- payment.php
+++ payment.php
@@ -1,5 +1,28 @@
+<?php
+include("connect.php");
+
+if (isset($_GET['id'])) {
+    $hotel_id = $_GET['id'];
+
+    $query = "SELECT * FROM hotels WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $hotel_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $hotel = $result->fetch_assoc();
+    } else {
+        die("Hotel tidak ditemukan.");
+    }
+} else {
+    die("ID Hotel tidak ditemukan.");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,8 +33,9 @@
     <script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
 
 </head>
+
 <body>
-    
+
     <div class="payment">
         <div class="left">
             <div class="hotel-img">
@@ -23,27 +47,36 @@
         </div>
 
         <div class="right">
-            <form action="POST">
-                <label for="nama">Nama : </label>
-                <input type="text" placeholder="Arya Wiratama" id="nama">
+            <form action="process_payment.php" method="POST">
+                <input type="hidden" name="hotel_id" value="<?php echo $hotel['id']; ?>">
+                <input type="hidden" name="price" value="<?php echo $hotel['price']; ?>">
+                <input type="hidden" name="image_url" value="<?php echo $hotel['image_url'];?>">
 
-                <label for="email">Email : </label>
-                <input type="email" placeholder="arya@gmail.com" id="email">
+                <label for="email">Email :</label>
+                <input type="email" placeholder="Your Email" id="email" name="email" required>
 
-                <label for="night">Night : </label>
-                <input type="number" placeholder="3" id="night" value="1">
+                <label for="name">Nama :</label>
+                <input type="text" placeholder="Your Name" id="name" name="name" required>
 
-                <label for="phone">Phone Number</label>
-                <input type="number" id="phone" placeholder="081283031559">
+                <label for="night">Night :</label>
+                <input type="number" placeholder="3" id="night" name="night" value="1" required>
 
-                <h2 id="total-Harga">Rp. {total-Harga}</h2>
+                <label for="payment_method">Payment Method:</label>
+                <select type="payment_method" id="payment_method" placeholder="Choose your Payment Method!"
+                    name="payment_method" required>
+                    <option type="payment_method" value="" disabled selected>Select Payment Method</option>
+                    <option value="Bank">Bank</option>
+                    <option value="E-Money">E-Money</option>
+                </select>
 
-                <button><a href="transaction.html" id="bayar">Bayar Sekarang!!!</a></button>
+                <label for="total_harga"><?php echo $hotel['price']; ?></label>
 
+                <button type="submit" id="bayar">Bayar Sekarang!</button>
             </form>
         </div>
     </div>
 
     <script src="script/payment.js"></script>
 </body>
+
 </html>
