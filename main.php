61d662fd972d368d856cd45fc6e5d32afe0f25ff
farelaryaduta <farelminecraft450@gmail.com>
2024-12-30 12:03:02 +0700

    mengubah navbar pesanan

--- main.php
+++ main.php
@@ -50,7 +50,7 @@ include("hotel.php");
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#booking">Book</a></li>
-                <li><a href="#contact">Pesanan Anda</a></li>
+                <li><a href="order.php">Pesanan Anda</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#contact">Contact Us</a></li>
 
